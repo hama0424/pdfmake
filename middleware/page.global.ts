123f9b804b99f93e36350abc55d6f4143abf0c15
hama0424 <s.hamasaki0424@gmail.com>
2023-01-10 03:44:22 +0900

    add vuetify

---
+++ middleware/page.global.ts
@@ -0,0 +1,18 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    // console.log(to)
+    // // alert("pages.global")
+    // const getDepth = (path) => {
+    //   return path.split('/').filter(seg => seg.length > 0).length;
+    // }
+  
+    // const toDepth = getDepth(to.path);
+    // const fromDepth = getDepth(from.path);
+  
+    // if (toDepth > fromDepth) {
+    //   to.meta.pageTransition = { name: 'page-left' };
+    //   from.meta.pageTransition = { name: 'page-left' };
+    // } else {
+    //   to.meta.pageTransition = { name: 'page-right' };
+    //   from.meta.pageTransition = { name: 'page-right' };
+    // }
+})
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <div>test</div>
+    <v-btn>test btn</v-btn>
+</template>
